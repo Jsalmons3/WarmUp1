7bd7c2f0921fb2256148f3cd00ec035392a1ae38
Jeffrey Salmons <jsalmons3@student.cscc.edu>
2026-09-14 22:04:41 -0400

    Made the sphere place holder and put the cubes into a circle around the sphere and made it to where you can quit the game.

---
+++ Warmup1.py
@@ -0,0 +1,35 @@
+import math, sys, random
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.fighter = self.loader.loadModel('./Assets/sphere')
+        self.fighter.reparentTo(self.render)
+        self.fighter.setColor(1.0, 0.0, 0.0, 1.0)
+        
+        self.parent = self.loader.loadModel('./Assets/cube')
+
+        x = 0
+        for i in range(100):
+            theta = x
+            self.placeholder2 = self.render.attachNewNode('Placeholder2')
+            self.placeholder2.setPos(50.0 * math.cos(theta), 50.0 * math.sin(theta), 0.0 * math.tan(theta))
+            red = 0.6 + random.random() * 0.4
+            green = 0.6 + random.random() * 0.4
+            blue = 0.6 + random.random() * 0.4
+            self.placeholder2.setColorScale(red, green, blue, 1.0)
+            self.parent.instanceTo(self.placeholder2)
+            x = x + 0.06
+
+        self.accept('escape', self.quit)
+    def quit(self):
+        sys.exit()
+app = MyApp()
+app.run()
