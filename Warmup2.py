e7899df62c5bde18cdd889d14111f58626d4a7c0
Jeffrey Salmons <jsalmons3@student.cscc.edu>
2026-09-20 20:41:16 -0400

    Made it to where the sphere can move right

--- Warmup2.py
+++ Warmup2.py
@@ -36,6 +36,8 @@ class MyApp(ShowBase):
 
         self.accept('arrow_left', self.negativeX, [1])
         self.accept('arrow_left-up', self.negativeX, [0])
+        self.accept('arrow_right', self.positiveX, [1])
+        self.accept('arrow_right-up', self.positiveX, [0])
 
         self.accept('escape', self.quit)
     def quit(self):
@@ -52,6 +54,15 @@ class MyApp(ShowBase):
         self.fighter.setX(self.fighter, -1)
         return task.cont # Sets the task to continue next game cycle
 
-    
+    def positiveX(self, keyDown):
+        if (keyDown):
+            self.taskMgr.add(self.movePositiveX, 'movePositiveX')
+        
+        else:
+            self.taskMgr.remove('movePositiveX')
+
+    def movePositiveX(self, task):
+        self.fighter.setX(self.fighter, 1)
+        return task.cont
 app = MyApp()
 app.run()
